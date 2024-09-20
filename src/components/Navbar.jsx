07eb891c8a65fe5fe007746d7dbbcff34b24b195
justmacn <172618812+justmacn@users.noreply.github.com>
2024-09-20 13:48:08 -0400

    added styling to header, needs debugging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
+// import components from chakra ui
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Box, HStack, Button } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+import { Box, HStack, Button, Image } from '@chakra-ui/react';
+import './Navbar.css';
 
+
+// Nvabar componenet
 const Navbar = () => {
   return (
-    <Box as="nav" p={4} bg="gray.800" color="white">
-      <HStack spacing={8} justify="center">
-        <Button as={Link} to="/" variant="ghost" colorScheme="whiteAlpha">Home</Button>
-        <Button as={Link} to="/work" variant="ghost" colorScheme="whiteAlpha">Work</Button>
-        <Button as={Link} to="/about" variant="ghost" colorScheme="whiteAlpha">About</Button>
-        <Button as={Link} to="/contact" variant="ghost" colorScheme="whiteAlpha">Contact</Button>
+    <Box as="nav" p={4} className='navBox'>
+      <Image boxSize='75px' objectFit='cover' mx={5} mt={2} src='/assets/images/jmLogo/jm-logo@2x.png' alt='JMcX logo' className='logo' />
+      <HStack spacing={8} justify="right" mx={10} mt={4}>
+        <Button as={NavLink} to="/" variant="link" className={({isActive}) => (isActive ? 'navBtn activeNav' : 'navBtn')}>Home</Button>
+        <Button as={NavLink} to="/work" variant="link" className='navBtn' activeClassName='activeNav'>Work</Button>
+        <Button as={NavLink} to="/about" variant="link" className='navBtn' activeClassName='activeNav'>About</Button>
+        <Button as={NavLink} to="/contact" variant="link" className='navBtn' activeClassName='activeNav'>Contact</Button>
       </HStack>
     </Box>
   );
