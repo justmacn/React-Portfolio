4e2d210a8aea82607276d980d739b668938fe63f
justmacn <172618812+justmacn@users.noreply.github.com>
2024-09-20 14:24:54 -0400

    debugged nav hover and active state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,82 @@ import { Box, HStack, Button, Image } from '@chakra-ui/react';
 import './Navbar.css';
 
 
-// Nvabar componenet
+// Nvabar componenet w/ style prop for active state
 const Navbar = () => {
   return (
     <Box as="nav" p={4} className='navBox'>
       <Image boxSize='75px' objectFit='cover' mx={5} mt={2} src='/assets/images/jmLogo/jm-logo@2x.png' alt='JMcX logo' className='logo' />
       <HStack spacing={8} justify="right" mx={10} mt={4}>
-        <Button as={NavLink} to="/" variant="link" className={({isActive}) => (isActive ? 'navBtn activeNav' : 'navBtn')}>Home</Button>
-        <Button as={NavLink} to="/work" variant="link" className='navBtn' activeClassName='activeNav'>Work</Button>
-        <Button as={NavLink} to="/about" variant="link" className='navBtn' activeClassName='activeNav'>About</Button>
-        <Button as={NavLink} to="/contact" variant="link" className='navBtn' activeClassName='activeNav'>Contact</Button>
-      </HStack>
+
+        {/* Home Tab */}
+        <Button as={NavLink} to="/" variant="link" className='navBtn'
+                sx={{
+                    // hover styling
+                    _hover: {
+                    color: 'goldenrod',
+                    fontWeight: '300',
+                    },
+                    // active styling
+                    _activeLink: {
+                    color: 'goldenrod',
+                    fontWeight: '400', 
+                    },
+                }}
+        >
+          Home
+        </Button>
+
+        {/* Work Button */}
+        <Button as={NavLink} to="/work" variant="link" className='navBtn'
+                sx={{
+                    _hover: {
+                    color: 'goldenrod',
+                    fontWeight: '300',
+                    },
+
+                    _activeLink: {
+                    color: 'goldenrod',
+                    fontWeight: '400', 
+                    },
+                }}
+        >
+          Work
+        </Button>
+
+        {/* About Button */}
+        <Button as={NavLink} to="/about" variant="link" className='navBtn'
+                sx={{
+                    _hover: {
+                    color: 'goldenrod',
+                    fontWeight: '300',
+                    },
+
+                    _activeLink: {
+                    color: 'goldenrod',
+                    fontWeight: '400', 
+                    },
+                }}
+        >
+          About
+        </Button>
+        
+        {/* Contact Button */}
+        <Button as={NavLink} to="/contact" variant="link" className='navBtn'
+                sx={{
+                    _hover: {
+                    color: 'goldenrod',
+                    fontWeight: '300',
+                    },
+
+                    _activeLink: {
+                    color: 'goldenrod',
+                    fontWeight: '400', 
+                    },
+                }}
+        >
+          Contact
+        </Button>
+        </HStack>
     </Box>
   );
 };
